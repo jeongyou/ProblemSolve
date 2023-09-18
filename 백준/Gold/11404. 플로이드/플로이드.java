0038859f94b3e8a7e2a8b55028f30070c19ffedd
youjeong Jeong <wjddbwjd1205@naver.com>
2023-09-19 00:18:35 +0900

    [Gold IV] Title: 플로이드, Time: 664 ms, Memory: 46368 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -4,36 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
         int[][] map = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                //무한대로 초기화
-                map[i][j] = 10000000; //최대 가중치 값 * 정점 개수 - 1 보다 큰 값 -> 100000 * 99 이상값
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                map[i][j] = 10000000;
             }
-            //자기 자신으로 향하는건 0으로
             map[i][i] = 0;
         }
 
-        StringTokenizer st;
-        for (int i = 0; i < m; i++) {
+        for(int i=0;i<m;i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken()) - 1;
             int b = Integer.parseInt(st.nextToken()) - 1;
             int c = Integer.parseInt(st.nextToken());
-            //시작 도시와 도착 도시를 연결하는 노선은 하나가 아닐 수 있다
+
             map[a][b] = Math.min(map[a][b], c);
         }
 
-        //플로이드워셜
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
                     map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
                 }
             }
@@ -41,7 +39,6 @@ public class Main {
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                //갈 수 없는 경우에는 0을 출력
                 if(map[i][j] == 10000000)
                     System.out.print("0 ");
                 else
