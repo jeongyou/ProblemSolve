8f24f9aa67de3d209300d1fc11edbc2dbea8f9d0
youjeong Jeong <wjddbwjd1205@naver.com>
2023-11-28 17:23:52 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,7 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt"
+let input = require("fs").readFileSync(filePath).toString().trim().split("\n")
+let __idx = 0
+let ini = () => Number(input[__idx++]) // input next integer
+let ins = () => input[__idx++].split(" ").map(Number) // input next split
+
+console.log(`${input[__idx++]}??!`)
