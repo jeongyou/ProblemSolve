7788ba87dc288079a232beb2497b65fcda622e5a
Youjeong Jeong <wjddbwjd1205@naver.com>
2026-02-13 21:42:15 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.39 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,12 @@
+function solution(food) {
+  let left = '';
+
+  for (let i = 1; i < food.length; i++) {
+    const count = Math.floor(food[i] / 2);
+    left += String(i).repeat(count);
+  }
+
+  const right = left.split('').reverse().join('');
+
+  return left + '0' + right;
+}
