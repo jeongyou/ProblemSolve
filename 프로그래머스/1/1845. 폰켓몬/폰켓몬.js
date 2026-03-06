aa9d4157b54dd9b3cc214b2bc0fe950ec7758cda
youjeong Jeong <wjddbwjd1205@naver.com>
2026-03-06 18:34:46 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,8 +1,7 @@
 function solution(nums) {
     const set = new Set(nums);
-    const can = 0;
     
-    const selectCnt = +(nums.length / 2);
+    const selectCnt = nums.length / 2;
     
     const type = set.size
     
